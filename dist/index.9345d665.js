c14a5962e786ab0319a08f77c360276d22161dd2
iisa-gallego <isabella.gallego2@correo.icesi.edu.co>
2022-05-27 00:44:38 -0500

    actualizo para no dañar el código

--- dist/index.9345d665.js
+++ dist/index.9345d665.js
@@ -1,13 +1,13 @@
 alert("entró");
 const bannerTitleAnimation = document.getElementById("banner__title");
-
-bannerTitleAnimation.addEventListener("mouseover", e => {
+bannerTitleAnimation.addEventListener("mouseover", (e)=>{
     console.log("in");
     bannerTitleAnimation.classList.add("animate__animated");
     bannerTitleAnimation.classList.add("animate__bounce");
 });
-
-bannerTitleAnimation.addEventListener("mouseout", e => {
+bannerTitleAnimation.addEventListener("mouseout", (e)=>{
     console.log("leave");
     bannerTitleAnimation.className = "title";
 });
+
+//# sourceMappingURL=index.9345d665.js.map
